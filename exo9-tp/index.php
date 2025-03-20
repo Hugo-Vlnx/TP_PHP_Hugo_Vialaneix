00f3bb99d7d7c70c50ade214526b83cbbc40ffe4
Hugo.V <hugo.vialaneix@gmail.com>
2025-03-20 15:50:43 +0100

    part2 avec bonus

--- exo9-tp/index.php
+++ exo9-tp/index.php
@@ -3,28 +3,20 @@
 
 
 $etudiant = $dbPDO->prepare("SELECT * FROM etudiants");
-
-
 $etudiant->execute();
-
-
 $etudiants = $etudiant->fetchAll();
-
 echo '<h1>'."liste des éleves:".'</h1>'.'<br>';
-
 echo '<ul>';
 foreach ($etudiants as $etudiant) {
     echo '<li>'.$etudiant['id'].' - '.$etudiant['prenom'].' '.$etudiant['nom'].'</li>';
 }
 echo '</ul>';
 
+
+
 echo '<h1>'."liste des Classes:".'</h1>'.'<br>';
 $classe = $dbPDO->prepare("SELECT * FROM classes");
-
-
 $classe->execute();
-
-
 $classes = $classe->fetchAll();
 echo '<ul>';
 foreach ($classes as $classe) {
@@ -32,17 +24,29 @@ foreach ($classes as $classe) {
 }
 echo '</ul>';
 
+
 echo '<h1>'."liste des Porfesseurs:".'</h1>'.'<br>';
 $professeur = $dbPDO->prepare("SELECT * FROM professeurs");
-
-
 $professeur->execute();
-
-
 $professeurs = $professeur->fetchAll();
 echo '<ul>';
 foreach ($professeurs as $professeur) {
     echo '<li>'.$professeur['id'].' - '.$professeur['prenom'].'  '.$professeur['nom'].'</li>';
 }
 echo '</ul>';
+
+echo '<h1>'."liste des Porfesseurs detaillée:".'</h1>'.'<br>';
+$bonus = $dbPDO->prepare("SELECT professeurs.nom, professeurs.prenom, matiere.lib AS matiere, classes.libelle AS classe
+                          FROM professeurs
+                          JOIN matiere ON professeurs.id_matiere = matiere.id
+                          JOIN classes ON professeurs.id_classe = classes.id");
+$bonus->execute();
+$profs = $bonus->fetchAll();
+echo '<ul>';
+foreach ($profs as $prof) {
+    echo '<li>'.$prof['prenom'].' '.$prof['nom'].' '.$prof['matiere'].' '.$prof['classe'].'</li>';
+}
+echo '</ul>';
+
+
 ?>
